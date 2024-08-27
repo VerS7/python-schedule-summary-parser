e2c832cf69acf029bfa8ebef16b1313131e4ddc2
Albert <thegoversus@gmail.com>
2024-08-27 13:57:04 +0300

    add parse all Parse summary parses only necessary information

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -14,7 +14,11 @@ def format_data(parsed: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     result = []
 
     for elem in parsed:
-        result.append({**elem["data"], "отрезок": int(f"{elem['from']}{elem['to']}"), "семестр": elem["semester"]})
+        if isinstance(elem["data"], list):
+            for fragment in elem["data"]:
+                result.append({**fragment, "отрезок": int(f"{elem['from']}{elem['to']}"), "семестр": elem["semester"]})
+        else:
+            result.append({**elem["data"], "отрезок": int(f"{elem['from']}{elem['to']}"), "семестр": elem["semester"]})
     return result
 
 
@@ -36,15 +40,17 @@ class Summary:
         response = self._get_response(self._url + "/vp.htm")
         soup = BeautifulSoup(response.text, "html.parser")
         teachers = []
+
         for row in soup.find("table", {"class": "inf"}).find_all("a"):
-            teachers.append({"ФИО": _decode(row.text),
-                             "URL": self._url + f"/{row.attrs['href']}"})
+            teachers.append({"ФИО": _decode(row.get_text()), "URL": self._url + f"/{row.attrs['href']}"})
+
         return teachers
 
     def parse_summary(self) -> List[Dict[str, Any]]:
-        """Парсит всю информацию по всем преподам"""
+        """Парсит основную информацию по всем преподам"""
         teachers = self.parse_all_teachers()
         summary = []
+
         for teacher in teachers:
             t_data = self._parse_total(self._get_response(teacher["URL"]))
 
@@ -59,12 +65,30 @@ class Summary:
                                      "Кол-во групп": len(groups),
                                      "Дисциплины": disciples,
                                      "Кол-во дисциплин": len(disciples),
-                                     "Сумм. часов": sum([d["Часы"] for d in t_data])}})
+                                     "Сумм. часов": sum([d["Всего, ч"] for d in t_data])}})
+
         return summary
 
+    def parse_all(self) -> List[Dict[str, Any]]:
+        """Парсит всю информацию по всем преподам"""
+        teachers = self.parse_all_teachers()
+        total = []
+
+        for teacher in teachers:
+            total.append({"from": self.from_,
+                          "to": self.to_,
+                          "semester": self.semester,
+                          "data": self._parse_total(self._get_response(teacher["URL"]))
+                          })
+        return total
+
     def parse_format_summary(self) -> List[Dict[str, Any]]:
-        """Форматирует данные с итогов в единый список для экспорта"""
+        """Форматирует основные данные с итогов в единый список для экспорта"""
         return format_data(self.parse_summary())
+
+    def parse_format_all(self) -> List[Dict[str, Any]]:
+        """Форматирует все данные с итогов в единый список для экспорта"""
+        return format_data(self.parse_all())
 
     def _get_response(self, url: str) -> Response:
         response = self._session.post(url)
@@ -76,15 +100,25 @@ class Summary:
 
         return response
 
-    def _parse_total(self, response: Response) -> List[Dict[str, str]]:
+    def _parse_total(self, response: Response) -> List[Dict[str, Any]]:
         soup = BeautifulSoup(response.text, "html.parser")
         total = []
+
         for row in soup.find("table", {"class": "inf"}).find_all("tr")[1::]:
             data = row.find_all("td")
-            total.append({"ФИО": _decode(data[1].text),
-                          "Группа": _decode(data[2].text),
-                          "Дисциплина": _decode(data[4].text),
-                          "Часы": int(data[6].text)})
+
+            total.append({"ФИО": _decode(data[1].get_text()),
+                          "Группа": _decode(data[2].get_text()),
+                          "П/г": int(data[3].get_text()),
+                          "Дисциплина": _decode(data[4].get_text()),
+                          "Тип": _decode(data[5].get_text()),
+                          "Всего, ч": int(data[6].get_text()),
+                          "План, ч": int(data[7].get_text()),
+                          "Остаток, ч": int(data[8].get_text()),
+                          "План 2н, ч": float(data[10].get_text().replace(",", ".")),
+                          "Факт 2н, ч": int(data[11].get_text()),
+                          "Окончание": data[12].get_text()
+                          })
         return total
 
     def _generate_url(self) -> str:
@@ -100,7 +134,7 @@ class SummaryRetrospective:
         self._session = session
 
     def parse_retrospective(self) -> List[Dict[str, Any]]:
-        """Парсит всю информацию по всем преподам и всем семестрам за определённый период"""
+        """Парсит основную информацию по всем преподам и всем семестрам за определённый период"""
         summaries = []
 
         for i in range(self.from_, self.to_):
@@ -112,6 +146,23 @@ class SummaryRetrospective:
 
         return summaries
 
+    def parse_retrospective_all(self) -> List[Dict[str, Any]]:
+        """Парсит всю информацию по всем преподам и всем семестрам за определённый период"""
+        totals = []
+
+        for i in range(self.from_, self.to_):
+            try:
+                totals.extend(Summary(i, i + 1, 1, session=self._session).parse_all())
+                totals.extend(Summary(i, i + 1, 2, session=self._session).parse_all())
+            except HTTPError:
+                continue
+
+        return totals
+
     def parse_format_retrospective(self) -> List[Dict[str, Any]]:
-        """Форматирует данные с ретроспективы в единый список для экспорта"""
+        """Форматирует ретроспективу основных данных в единый список для экспорта"""
         return format_data(self.parse_retrospective())
+
+    def parse_format_retrospective_all(self) -> List[Dict[str, Any]]:
+        """Форматирует ретроспективу всех данных в единый список для экспорта"""
+        return format_data(self.parse_retrospective_all())
